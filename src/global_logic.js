8a415cab4cebbc83a4640627d6802682bd3eed27
エッケザックス <kagekiyo7000@nifty.com>
2019-10-10 16:47:16 +0900

    Change to djeeta's default ougiGage

--- src/global_logic.js
+++ src/global_logic.js
@@ -1033,7 +1033,7 @@ module.exports.calcBasedOneSummon = function (summonind, prof, buff, totals) {
             ougiGageLimit: (totals[key]["job"]["name"] == "剣豪" ||totals[key]["job"]["name"] == "侍" || key == "ヴァジラ" || key == "サーヴァンツ ドロシー＆クラウディア" 
             || key == "[最終]オクトー" || key == "オクトー" || key == "サビルバラ(イベントver)" || key == "サビルバラ" 
             || key == "ジン(克己浪人)" || key == "ジン(風属性ver)" || key == "ミリン" || key == "ミリン(光属性ver)") ? 200 : 100,
-            ougiGage: 30,
+            ougiGage: (key == "Djeeta") ? 100 : 30,
             attackMode: "",
         };
     }
