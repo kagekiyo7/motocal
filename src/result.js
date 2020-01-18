79d1ca5dead6d8fbbec8de629cccd138cfeb1073
kei <45387555+kei-gbf@users.noreply.github.com>
2020-01-19 01:55:46 +0900

    Revert "Change overused map to for-loop"
    This reverts commit f48f23260ce14ae7697f2a78376725db5f87983d.

--- src/result.js
+++ src/result.js
@@ -239,13 +239,8 @@ var ResultList = CreateClass({
         }
     },
     getInitialSwitchState: function (_switchList=switchList) {
-        const res = {};
-        for (const {items} of _switchList) {
-            for (const {name, selected} of items) {
-                res[name] = selected || false;
-            }
-        }
-        return res;
+        return Object.assign(..._switchList.map(({items}) =>
+            Object.fromEntries(items.map(({name, selected}) => [name, selected||false]))));
     },
     getInitialState: function () {
         return Object.assign({
